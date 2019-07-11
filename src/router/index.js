98a3b9081f4e0610cd08e550814989b602ee2908
Knight <177034@qq.com>
2019-07-11 14:32:19 +0800

    初始化login

---
+++ src/router/index.js
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import login from '@/views/login'
+Vue.use(VueRouter)
+const router = new VueRouter({
+  routes: [{ name: 'login', path: '/login', component: login }]
+})
+export default router
---
+++ src/views/login.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="container">
+        <el-card class="container-box">
+            <img src="" alt="">
+        </el-card>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="less">
+.container{
+    width: 100%;
+    height: 100%;
+    background-color: green;
+    position: absolute;
+    // background: url('../../');
+    .container-box{
+        width: 400px;
+        height: 300px;
+        position:absolute;
+        left: 50%;
+        top:50%;
+        transform: translate(-50%,-50%);
+        img{
+
+        }
+    }
+}
+</style>
